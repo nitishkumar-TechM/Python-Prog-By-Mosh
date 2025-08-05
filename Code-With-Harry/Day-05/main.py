22a3eef0b163ffe037520974fd0ec21d8db0bdb8
nitishkumar-TechM <nkcm12345@gmail.com>
2025-08-05 10:54:24 +0530

    print function in detail

---
+++ Code-With-Harry/Day-05/main.py
@@ -0,0 +1,6 @@
+#This is a comment
+print('Nitish \nkumar')
+print('hey', 6, 7, 'nitish', sep='~')
+print('hey', 6, 7, 'nitish', sep='~', end='009\n')
+# end = 'end': Specify what to print at the end. Default is '\n' (line feed)
+print('TechMahindra')
