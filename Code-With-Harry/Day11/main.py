0d44baa2b4f5791cbd84428acc4a4349e5b930b5
nitishkumar-TechM <nkcm12345@gmail.com>
2025-08-05 15:39:46 +0530

    Strings in python basic intro

---
+++ Code-With-Harry/Day11/main.py
@@ -0,0 +1,15 @@
+name = "Nitish"
+friend = "Rahul"
+
+print('Hello, ' + name)
+
+print('''Hi i am nitish
+who are you''')    #print as it is
+
+print(name[0])
+print(name[1])
+
+# In Python String is like of Array of Characters
+print("-----------------------")
+for char in name:
+    print(char)
