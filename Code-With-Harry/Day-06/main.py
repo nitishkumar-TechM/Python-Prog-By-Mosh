82d8ef91a1ffa5c6a5cf00edf78524ccec9a2c80
nitishkumar-TechM <nkcm12345@gmail.com>
2025-08-05 11:38:58 +0530

    Variables and Data types

---
+++ Code-With-Harry/Day-06/main.py
@@ -0,0 +1,23 @@
+# Variable
+
+a = 1
+print(a)
+b = "Nitish"
+print(b)
+c = True
+print(type(c))
+
+z = complex(6, 4)
+print('Complex number: ', z)
+print(type(z))
+
+#Sequence data : list, tuple
+
+list1 = [8, 2.3, [-4,5], ["apple", "banana"]]
+print(list1)
+
+tuple1 = (('parrot', 'sparrow'), ('lion', 'tiger'))
+print(tuple1)
+
+dict1 = {"name": "Nitish", "age": 25, "canVote": True }
+print(dict1)
