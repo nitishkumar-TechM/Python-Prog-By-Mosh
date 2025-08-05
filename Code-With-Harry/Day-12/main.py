26158bd759b8cb6fd0f5fc4bb1faab8a2101cf5a
nitishkumar-TechM <nkcm12345@gmail.com>
2025-08-05 17:29:16 +0530

    Strings Slicing and Operations on String

---
+++ Code-With-Harry/Day-12/main.py
@@ -0,0 +1,22 @@
+#String Slicing and Operations on String
+
+name = "Nitish Kumar"
+
+print(name[:6])
+print(name[:6:2])
+print(name[6:])
+print(name[0:-3])
+print(name[0: len(name)-3])
+
+print(len(name))
+
+print('-------------------')
+
+fruit = "MangoBanana"
+
+print('Length of fruit: ', len(fruit))
+
+print(fruit[-1:-3])
+print(fruit[len(fruit)-1: len(fruit)-3])
+print(fruit[-3:-1])
+
