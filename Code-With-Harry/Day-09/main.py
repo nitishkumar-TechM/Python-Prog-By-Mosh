5d7ff00baa29675a2960183d88d5d99e0c539033
nitishkumar-TechM <nkcm12345@gmail.com>
2025-08-05 12:47:16 +0530

    Typecasting in python

---
+++ Code-With-Harry/Day-09/main.py
@@ -0,0 +1,20 @@
+# TypeCasting
+
+a = "1"
+
+b = "2"
+
+print(int(a) + int(b))  # Explicit Type conversion
+
+#Implicit Type casting
+
+c = 1.9
+d = 4
+
+result = c + d
+print('type of result:', type(result))
+print('result:', result)
+
+isVote = True;
+res = int(isVote)
+print('res', res)
